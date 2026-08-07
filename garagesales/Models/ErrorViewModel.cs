129b3670702813b4d20300871d0baed27d0abad4
Thomas Alcorn <talcor7@wgu.edu>
2026-08-07 19:03:28 -0400

    Add project files.

---
+++ garagesales/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace garagesales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
