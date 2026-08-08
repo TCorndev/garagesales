408223f7a9719374c216d7eb551d0ac5b7ba0022
Thomas Alcorn <talcor7@wgu.edu>
2026-08-08 11:56:03 -0400

    Testing sql connection

--- garagesales/Program.cs
+++ garagesales/Program.cs
@@ -1,4 +1,20 @@
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Console.WriteLine(builder.Configuration.GetConnectionString("GarageSaleDatabase"));
+
+try
+{
+    using var connection = new SqlConnection(builder.Configuration.GetConnectionString("GarageSaleDatabase"));
+    connection.Open();
+
+    Console.WriteLine("Success");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex}");
+}
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
